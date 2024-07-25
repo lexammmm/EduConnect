e1554234d94c5d9c94d43ea4c130b0a3663148d4
lexammmm <163344837+lexammmm@users.noreply.github.com>
2024-07-25 10:52:27 +0300

    optimization improvement

--- main.go
+++ main.go
@@ -5,8 +5,10 @@ import (
 	"github.com/gin-gonic/gin"
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/postgres"
-	"os"
 	"github.com/joho/godotenv"
+	"os"
+	"sync"
+	"time"
 )
 
 type Course struct {
@@ -16,6 +18,15 @@ type Course struct {
 
 var db *gorm.DB
 var err error
+
+// Cache structure
+type courseCache struct {
+	courses []Course
+	lastUpdate time.Time
+	mu sync.RWMutex
+}
+
+var cCache courseCache
 
 func initDB() {
 	err := godotenv.Load()
@@ -45,15 +56,32 @@ func createCourse(c *gin.Context) {
 	var course Course
 	c.BindJSON(&course)
 	db.Create(&course)
+	// Invalidate the cache
+	cCache.mu.Lock()
+	cCache.courses = nil // Clear the cache to force a refresh on next read
+	cCache.mu.Unlock()
 	c.JSON(200, course)
 }
 
 func getCourses(c *gin.Context) {
+	cCache.mu.RLock()
+	if time.Since(cCache.lastUpdate) < 5*time.Minute && cCache.courses != nil {
+		// Serve from cache
+		c.JSON(200, cCache.courses)
+		cCache.mu.RUnlock()
+		return
+	}
+	cCache.mu.RUnlock()
+
 	var courses []Course
 	if err := db.Find(&courses).Error; err != nil {
 		c.AbortWithStatus(404)
 		fmt.Println(err)
 	} else {
+		cCache.mu.Lock()
+		cCache.courses = courses
+		cCache.lastUpdate = time.Now()
+		cCache.mu.Unlock()
 		c.JSON(200, courses)
 	}
 }
