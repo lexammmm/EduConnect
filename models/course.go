f6ef5e894b624813272e8862db5de2299157b0bf
lexammmm <163344837+lexammmm@users.noreply.github.com>
2024-08-24 17:40:28 +0300

    optimize the naming

--- models/course.go
+++ models/course.go
@@ -1,28 +1,44 @@
 package main
 
 import (
-	"gorm.io/driver/postgres"
-	"gorm.io/gorm"
-	"os"
+    "gorm.io/driver/postgres"
+    "gorm.io/gorm"
+    "os"
 )
 
-func init() {
-	dbURL := os.Getenv("DATABASE_URL")
-	db, err := gorm.Open(postgres.Open(dbURL), &gorm.Config{})
-	if err != nil {
-		panic("failed to connect database")
-	}
+var db *gorm.DB
 
-	db.AutoMigrate(&Course{})
+func init() {
+    dbURL := os.Getenv("DATABASE_URL")
+    var err error
+    db, err = gorm.Open(postgres.Open(dbURL), &gorm.Config{})
+    if err != nil {
+        panic("failed to connect database")
+    }
+
+    err = db.AutoMigrate(&Course{})
+    if err != nil {
+        panic("failed to auto migrate")
+    }
 }
 
 type Course struct {
-	gorm.Model
-	ID          uint   `gorm:"primaryKey"`
-	Title       string `gorm:"size:255;not null"`
-	Description string `gorm:"size:1024;not null"`
-	Instructor  string `gorm:"size:255;not null"`
+    gorm.Model
+    ID          uint   `gorm:"primaryKey"`
+    Title       string `gorm:"size:255;not null"`
+    Description string `gorm:"size:1024;not null"`
+    Instructor  string `gorm:"size:255;not null"`
 }
 
 func main() {
+}
+
+func closeDB() {
+    sqlDB, err := db.DB()
+    if err != nil {
+        panic("failed to get database object from GORM connection")
+    }
+    if err := sqlDB.Close(); err != nil {
+        panic("failed to close database connection")
+    }
 }
