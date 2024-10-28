81ae7da42862eea32581a7f520107d35c3321f29
lexammmm <163344837+lexammmm@users.noreply.github.com>
2024-10-28 19:22:54 +0200

    change variable

--- database/db.go
+++ database/db.go
@@ -1,94 +1,94 @@
 package main
 
 import (
-	"encoding/json"
-	"fmt"
-	"log"
-	"net/http"
-	"os"
-	"strconv"
+    "encoding/json"
+    "fmt"
+    "log"
+    "net/http"
+    "os"
+    "strconv"
 
-	"github.com/gorilla/mux"
-	"github.com/joho/godotenv"
-	"gorm.io/driver/postgres"
-	"gorm.io/gorm"
+    "github.com/gorilla/mux"
+    "github.com/joho/godotenv"
+    "gorm.io/driver/postgres"
+    "gorm.io/gorm"
 )
 
 type Course struct {
-	gorm.Model
-	Title       string `json:"title"`
-	Description string `json:"description"`
+    gorm.Model
+    Title       string `json:"title"`
+    Description string `json:"description"`
 }
 
-var DB *gorm.DB
+var databaseInstance *gorm.DB
 
 func init() {
-	loadEnv()
-	setupDatabase()
+    loadEnvironmentVariables()
+    initializeDatabaseConnection()
 }
 
-func loadEnv() {
-	if err := godotenv.Load(); err != nil {
-		log.Fatal("Error loading .env file:", err)
-	}
+func loadEnvironmentVariables() {
+    if err := godotenv.Load(); err != nil {
+        log.Fatal("Error loading .env file:", err)
+    }
 }
 
-func setupDatabase() {
-	username := os.Getenv("DB_USER")
-	password := os.Getenv("DB_PASS")
-	dbName := os.Getenv("DB_NAME")
-	dbHost := os.Getenv("DB_HOST")
-	dbPort := os.Getenv("DB_PORT")
+func initializeDatabaseConnection() {
+    username := os.Getenv("DB_USER")
+    password := os.Getenv("DB_PASS")
+    dbName := os.Getenv("DB_NAME")
+    dbHost := os.Getenv("DB_HOST")
+    dbPort := os.Getenv("DB_PORT")
 
-	dsn := fmt.Sprintf("host=%s port=%s user=%s dbname=%s sslmode=disable password=%s", dbHost, dbPort, username, dbName, password)
+    dataSourceName := fmt.Sprintf("host=%s port=%s user=%s dbname=%s sslmode=disable password=%s", dbHost, dbPort, username, dbName, password)
 
-	database, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
-	if err != nil {
-		log.Fatalf("Failed to connect to database: %v", err)
-	}
+    database, err := gorm.Open(postgres.Open(dataSourceName), &gorm.Config{})
+    if err != nil {
+        log.Fatalf("Failed to connect to database: %v", err)
+    }
 
-	database.AutoMigrate(&Course{})
-	DB = database
+    database.AutoMigrate(&Course{})
+    databaseInstance = database
 }
 
 func main() {
-	r := mux.NewRouter()
-	api := r.PathPrefix("/api/v1").Subrouter()
+    router := mux.NewRouter()
+    apiRouter := router.PathPrefix("/api/v1").Subrouter()
 
-	api.HandleFunc("/courses", getCourses).Methods("GET")
-	api.HandleFunc("/course/{id}", getCourse).Methods("GET")
-	api.HandleFunc("/course", addCourse).Methods("POST")
+    apiRouter.HandleFunc("/courses", retrieveCoursesHandler).Methods("GET")
+    apiRouter.HandleFunc("/course/{id}", retrieveCourseHandler).Methods("GET")
+    apiRouter.HandleFunc("/course", createCourseHandler).Methods("POST")
 
-	log.Fatal(http.ListenAndServe(":8080", r))
+    log.Fatal(http.ListenAndServe(":8080", router))
 }
 
-func getCourses(w http.ResponseWriter, r *http.Request) {
-	var courses []Course
-	DB.Find(&courses)
-	respondWithJSON(w, courses)
+func retrieveCoursesHandler(responseWriter http.ResponseWriter, request *http.Request) {
+    var courses []Course
+    databaseInstance.Find(&courses)
+    respondWithJSON(responseWriter, courses)
 }
 
-func getCourse(w http.ResponseWriter, r *http.Request) {
-	params := mux.Vars(r)
-	id, _ := strconv.Atoi(params["id"])
-	var course Course
-	if err := DB.First(&course, id).Error; err != nil {
-		http.Error(w, "Course not found", http.StatusNotFound)
-		return
-	}
-	respondWithJSON(w, course)
+func retrieveCourseHandler(responseWriter http.ResponseWriter, request *http.Request) {
+    parameters := mux.Vars(request)
+    id, _ := strconv.Atoi(parameters["id"])
+    var course Course
+    if err := databaseInstance.First(&course, id).Error; err != nil {
+        http.Error(responseWriter, "Course not found", http.StatusNotFound)
+        return
+    }
+    respondWithJSON(responseWriter, course)
 }
 
-func addCourse(w http.ResponseWriter, r *http.Request) {
-	var course Course
-	if err := json.NewDecoder(r.Body).Decode(&course); err != nil {
-		http.Error(w, "Invalid request payload", http.StatusBadRequest)
-		return
-	}
-	DB.Create(&course)
-	respondWithJSON(w, course)
+func createCourseHandler(responseWriter http.ResponseWriter, request *http.Request) {
+    var course Course
+    if err := json.NewDecoder(request.Body).Decode(&course); err != nil {
+        http.Error(responseWriter, "Invalid request payload", http.StatusBadRequest)
+        return
+    }
+    databaseInstance.Create(&course)
+    respondWithJSON(responseWriter, course)
 }
 
 func respondWithJSON(w http.ResponseWriter, data interface{}) {
-	json.NewEncoder(w).Encode(data)
+    json.NewEncoder(w).Encode(data)
 }
